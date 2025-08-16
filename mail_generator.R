b37c78e527de5df33dfd2c42bd26f3e819e65e0a
Tobias Hector <tobias.hector@hotmail.com>
2025-08-16 17:08:07 +0200

    Added lines and fixed bug

--- mail_generator.R
+++ mail_generator.R
@@ -9,10 +9,13 @@ for (indic in indics) {
   table <- kable(indic, format = "html")
   part <- glue("
   <span style='color:blue; font-weight:bold;'>{names(indics)[index]}</span>
-  {table}
-  {names(indics)[number]} grew by <span style='background-color:#90EE90;'>2.3%</span> compared to 2022.
+               {table}
+  
+  {names(indics)[index]} grew by <span style='background-color:#90EE90;'>2.3%</span> compared to 2022.
   
   [See full dataset here]({links[[index]]})
+  
+  --------------------------------------------------------
   ")
   body_text <- paste0(body_text, "\n\n", part)
   
